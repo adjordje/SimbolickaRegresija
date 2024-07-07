04966dceb55351a5ba6258c5846827fbe0db627d
Djordje Aksentijevic <126694198+adjordje@users.noreply.github.com>
2024-07-07 19:46:14 +0200

    Add files via upload

--- ri.py
+++ ri.py
@@ -1,8 +1,9 @@
 import random
 import numpy as np
 from math import cos, sin
+from tqdm import tqdm
 import matplotlib.pyplot as plt
-
+import pandas as pd
 # Klasa kojom je predstavljen cvor (operacija ili vrednost)
 class Node:
     def __init__(self, operation=None, left=None, right=None, value=None):
@@ -13,7 +14,9 @@ class Node:
     # funkcija kojom se evaluira vrednost iz tekuceg cvora
     def evaluate(self, x):
         if self.operation is None:
-            return self.value * x if self.value is not None else x
+            if self.value is None:
+                return x
+            return self.value * x
         elif self.operation == "+":
             return self.left.evaluate(x) + self.right.evaluate(x)
         elif self.operation == "-":
@@ -21,7 +24,10 @@ class Node:
         elif self.operation == "*":
             return self.left.evaluate(x) * self.right.evaluate(x)
         elif self.operation == "/":
-            return self.left.evaluate(x) / self.right.evaluate(x)
+            b = self.right.evaluate(x)
+            if b == 0:
+                b = 0.000001
+            return self.left.evaluate(x) / b
         elif self.operation == "cos+":
             return cos(self.left.evaluate(x) + self.right.evaluate(x))
         elif self.operation == "cos-":
@@ -38,6 +44,15 @@ class Node:
             return sin(self.left.evaluate(x) * self.right.evaluate(x))
         elif self.operation == "sin/":
             return sin(self.left.evaluate(x) / self.right.evaluate(x))
+        elif self.operation == "l^":
+            return self.left.evaluate(x)**2 + self.right.evaluate(x)
+        elif self.operation == "r^":
+            return self.left.evaluate(x) + self.right.evaluate(x)**3
+        elif self.operation == "X":
+            if self.value is None:
+                return x
+            else:
+                return x
         else:
             raise ValueError("Nepodrzana operacija")
     # reprezentacija u stringu
@@ -50,10 +65,9 @@ class Node:
 # funkcija za generisanje izraza na osnovu tokena
 def generate_expression(depth):
     if depth == 0:
-        return Node(value=random.uniform(-100, 100))
+        return Node(value=random.uniform(-1000, 1000))
     else:
-        op = random.choice(["+", "-", "*", "/", "cos+",
-                            "sin+"])
+        op = random.choice(["+", "*", "l^", "r^", "-", "/", "cos+", "sin+", "X"])
         left = generate_expression(depth - 1)
         right = generate_expression(depth - 1)
         return Node(operation=op, left=left, right=right)
@@ -83,17 +97,18 @@ def crossover(parent1, parent2):
 
 # funkcija koja vrsi mutaciju nad genima u hromozomu
 def mutate(individual):
-    if random.random() < 0.4:  # Sansa mutacije
-        individual.left.operation = random.choice(["+", "-", "*", "/", "cos+", "sin+", "cos*"])
+    if random.random() < 0.8:  # Sansa mutacije
+        individual.left = generate_expression(depth=8) 
     else:
-        individual.right.value = random.uniform(-100, 100)
+        individual.right.value = random.uniform(-100000, 100000)
     return individual
 
 # funkcija koja vrsi evoluciju populacije
-def evolve(population, X, y, generations=1000):
-    for _ in range(generations):
+def evolve(population, X, y, generations=400):
+    for _ in tqdm(range(generations)):
         # izaberi roditelje
-        parents = sorted(random.sample(population, 2), key=lambda x: fitness(x, X, y), reverse=True)[:]
+        parents = sorted(random.sample(population, 2), key=lambda x: fitness(x, X, y))[:]
+
 
         # napravi novu jedinku kao kompoziciju roditelja
         offspring = crossover(*parents)
@@ -109,13 +124,43 @@ def evolve(population, X, y, generations=1000):
 
 # main funkcija
 if __name__ == "__main__":
-    X = np.random.rand(100, 1)
-    y = X + np.random.randn(100, 1) 
-    population_size = 500
-    depth = 6
+
+    # funkcija je (x-5)^3
+    # nsample = 400
+    # sig = 0.2
+    # x = np.linspace(-50, 50, nsample)
+    # X = np.column_stack((x/5, 10*np.sin(x), (x-5)**3, np.ones(nsample)))
+    # beta = [0.01, 1, 0.001, 5.]
+    # y_true = np.dot(X, beta)
+    # y = y_true + sig * np.random.normal(size=nsample)
+    # df = pd.DataFrame()
+    # df['x']=x
+    # df['y']=y
+
+
+    # point_color = '#852a5b'
+    # edgecolorsA = '#ffa600'
+    # edgecolorsB = '#73fffa'
+
+    # fig, (ax1, ax2) = plt.subplots(1,  2, figsize=(10,  5))
+
+    # ax1.scatter(df['x'], df['y'], c=point_color, edgecolors=edgecolorsA, alpha=1.0)
+    # ax2.scatter(df['x'], df['y'], c=point_color, edgecolors=edgecolorsA, alpha=1.0)
+    # plt.xlim(-30, 30)
+    # plt.ylim(-30,  30)
+    # plt.show()
+
+    
+
+    X = np.linspace(-50, 50, 100)
+    y = (X-5) ** 3 
+
+    population_size = 100
+    depth = 5
     population = generate_population(population_size, depth)
     evolved_population = evolve(population, X, y)
     best_individual = max(evolved_population, key=lambda x: fitness(x, X, y))
+    print(best_individual)
 
     X_values = X
     Y_values = [best_individual.evaluate(x) for x in X]
